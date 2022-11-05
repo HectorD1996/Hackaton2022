284eb5c919b5f30fb1c845a7cad2b855095a19ae
HectorD1996 <hectorfdl@hotmail.com>
2022-11-05 15:32:58 -0600

    Problema 3 cadena subsecuentes resuleto

---
+++ Problema3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+do
+{
+    string val;
+    string val2;
+    Console.Write("Enter string: ");
+    val = Console.ReadLine();
+
+    Console.Write("Enter string: ");
+    val2 = Console.ReadLine();
+
+    int mayor = Math.Max(val.Length, val.Length);
+    string result = "";
+
+    for (int i = 0; i < mayor; i++)
+    {
+        if (val[i] == val2[i])
+        {
+            result = result + val[i];
+        }
+    }
+
+    
+    Console.WriteLine(result);
+   
+    Console.WriteLine("Escriba exit para terminar, Cualquier tecla para continuar");
+} while (Console.ReadLine() != "exit");
